12aa1374c68234d05e4596891d759ac63e72d660
sism1s <marko.lazic880@gmail.com>
2021-02-26 17:47:23 +0000

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Test from "./Test";
+import TestReducer from "./TestReducer";
 
 function App() {
   return (
     <div>
       <Test />
+      <TestReducer />
     </div>
   );
 }
---
+++ src/TestReducer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useReducer } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+export default function Test() {
+  const [counter, dispatch] = useReducer(reducer, 0);
+
+  return (
+    <div>
+      <p>{counter}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>increment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>decrement</button>
+    </div>
+  );
+}

6f397b54899be3233d656f2e4957aadebc629f0f
sism1s <marko.lazic880@gmail.com>
2021-02-27 01:06:21 +0000

    Display Api data on UI

--- src/TestApi.js
+++ src/TestApi.js
@@ -1,28 +1,54 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import { useRef } from "react";
 import axios from "axios";
 
-const fetchRandomData = () => {
+const fetchRandomData = (pageNumber) => {
   return axios
-    .get("https://randomuser.me/api")
+    .get(`https://randomuser.me/api?page=${pageNumber}`)
     .then(({ data }) => {
-      return JSON.stringify(data);
+      return data;
     })
     .catch((err) => {
       console.log(err);
     });
 };
-fetchRandomData();
+
+const getFullUserName = (userInfo) => {
+  const {
+    name: { first, last },
+  } = userInfo;
+  return `${first} ${last}`;
+};
+
 export default function TestApi() {
-  const [data, setData] = useState([]);
+  const [userInfos, setUserInfos] = useState([]);
+  const [nextPageNumber, setNextPageNumber] = useState(1);
+
+  const fetchNextUser = useRef(() => {});
+
+  fetchNextUser.current = () => {
+    fetchRandomData(nextPageNumber).then((randomData) => {
+      const newUserInfos = [...userInfos, ...randomData.results];
+      setUserInfos(newUserInfos);
+      setNextPageNumber(randomData.info.page + 1);
+    });
+  };
 
   useEffect(() => {
-    fetchRandomData().then((randomData) => {
-      setData(randomData);
-      console.log(data);
-    });
+    fetchNextUser.current();
   }, []);
 
-  return <div>{data}</div>;
+  return (
+    <div>
+      <button onClick={() => fetchNextUser.current()}>Fetch next user</button>
+      {userInfos.map((userInfo, idx) => (
+        <div key={idx}>
+          <p>{getFullUserName(userInfo)}</p>
+          <img src={userInfo.picture.thumbnail} alt="personal" />
+        </div>
+      ))}
+    </div>
+  );
 }
